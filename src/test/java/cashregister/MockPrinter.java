8b81e0125174a1f506cde6f72711f9a237f5af4c
lvshudi <lvshudi119@126.com>
2019-08-26 17:03:34 +0800

    finish

---
+++ src/test/java/cashregister/MockPrinter.java
@@ -0,0 +1,15 @@
+package cashregister;
+
+public class MockPrinter extends Printer {
+	private String printThis = "";
+	
+	@Override
+	public void print(String printThis) {
+		super.print(printThis);
+		this.printThis = printThis;
+	}
+	
+	public String getPrintThis() {
+		return printThis;
+	}
+}
